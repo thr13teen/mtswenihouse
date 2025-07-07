097bc9d7959b472676d7eef959cb5ffe006ac970
mh <13beats@gmail.com>
2025-07-07 06:27:20 +0200

    Create 02.js

---
+++ mhpack/01js/00main/02.js
@@ -0,0 +1,12 @@
+
+
+function	showNavigation(){
+const		mhpt0 = document.getElementsByClass("mh")[0];
+			ind01.innerHTML +=`
+			<div class="mh-pt0">	<img src="mhpack/04images/mh01index/mh01index04.jpg"></div>
+			`
+			
+			}
+
+
+/*==========================*/
