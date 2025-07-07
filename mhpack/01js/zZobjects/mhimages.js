ef34a2a591ecd1926d6ba6ae24de7bccd35653f3
mh <13beats@gmail.com>
2025-07-07 06:27:49 +0200

    Create mhimages.js

---
+++ mhpack/01js/zZobjects/mhimages.js
@@ -0,0 +1,11 @@
+
+const		mhimages = [{ id:0,
+			link00: "index.html",			linkt01: "MH",
+			link01: "mhpack/nav04.html",	linkt02: "Heritage Hall",
+			link02: "mhpack/nav04.html", 	linkt03: "Peace Garden",
+			link03: "mhpack/nav04.html", 	linkt04: "The Fields",
+			link04: "mhpack/nav04.html", 	linkt05: "Legacy Court",
+			link05: "mhpack/nav04.html", 	linkt05: "The Pond"	}];
+const		img		= mhimageslink0[0];
+
+/*==========================*/
