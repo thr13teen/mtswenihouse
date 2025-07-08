b7e6af84bdc250337ad34660a6aecc9d696b536d
tHR13tEen <144280282+thr13teen@users.noreply.github.com>
2025-07-08 19:53:49 +0200

    Add files via upload

--- mhpack/01js/00main/00.js
+++ mhpack/01js/00main/00.js
@@ -1,31 +1,20 @@
 
-const		mhpt0 = document.getElementsByClassName("mh")[0];
-const 		mhbg	=	"mhpack/04images/mh00images/mh00.jpg";
-			mhpt0.innerHTML +=`
-			<div class="mhpt0">	<img src=${mhbg}></div>
-			`;
-
 const		mhpt1 = document.getElementsByClassName("mh")[0];
 			mhpt1.innerHTML +=`
-			<div class="mhpt1">	<div class="mh-pt1nav">	<p>&nbsp;</p></div>
-								<div class="mh-pt1nav">	<a href="">Mtsweni House</a></div>
-								<div class="mh-pt1nav">	<a href="">Heritage Hall</a> 
-														<a href="">Peace Garden</a> 
-														<a href="">The Fields</a> 
-														<a href="">Legacy Court</a></div>
-								<div class="mh-pt1nav">	<p>&nbsp;</p></div>
-			</div>
+
 			`;
 
 const		mhpt2 = document.getElementsByClassName("mh")[0];
 			mhpt2.innerHTML +=`
-			<div class="mhpt2">		<div class="mh-pt2splash">	<div class="mh-pt2splashhead"><p>Creating a home is an endevour,</p>
-																<p>pursued only by the hopeful.</p></div>
-									<div class="mh-pt2splashtext"><p>In the event you find yourself lost in the strives of life, refer to this document as a reminder of yourself.
-																	 The contents herein contain journal entries, guidelines and recommendations of how to return to self.
-																	 The wisdom contained herein is not one of a high education, but of a higher principle, wisdom in how to carry yourself.
-																	 For what more are we than kings unto ourselves.</p></div>
-																	 </div>
+			<div class="mhpt2">	<div class="mh-pt2splash">	<div class="mh-pt2splashhead">
+															<p>M</p>
+															</div>
+
+								<div class="mh-pt2splashtext">	<p>In the event you find yourself lost in the strives of life, refer to this document as a reminder of yourself.
+																The contents herein contain journal entries, guidelines and recommendations of how to return to self.
+																The wisdom contained herein is not one of a high education, but of a higher principle, wisdom in how to carry yourself.
+																For what more are we than kings unto ourselves.</p></div>
+																</div>
 			</div>
 			`;
 
